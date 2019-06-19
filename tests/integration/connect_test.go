4b4b624e443d0caab5f536e999fd81591a5d9fc1
David Beam <dbeam@neighborly.com>
2019-06-19 15:12:46 -0700

    added passing unit test for generate_address and test suite file for unit dir

---
+++ tests/integration/connect_test.go
@@ -0,0 +1,1 @@
+package pghelpers_test
---
+++ tests/unit/generate_address_test.go
@@ -0,0 +1,32 @@
+package pghelpers_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	pgh "github.com/neighborly/go-pghelpers"
+)
+
+var (
+	testTracingConfig = pgh.PostgresTracingConfig{true, true,
+		true, true, true,
+		true, true}
+	myTestString = "test"
+	testConfig   = pgh.PostgresConfig{
+		Host:       myTestString,
+		Port:       1,
+		Username:   myTestString,
+		Password:   myTestString,
+		Database:   myTestString,
+		SSLEnabled: false,
+		Tracing:    testTracingConfig}
+)
+
+var _ = Describe("RatingsDimension", func() {
+	It("Can generate ratings", func() {
+		//test generate address
+		result := testConfig.GenerateAddress()
+		Expect(result).To(Equal("host=test port=1 dbname=test user=test password=test sslmode=disable"))
+
+	})
+
+})
