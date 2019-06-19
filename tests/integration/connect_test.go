71e6c11f68ee175c2c70d139831743ae6ef55036
David Beam <dbeam@neighborly.com>
2019-06-19 15:53:08 -0700

    added integration test for connection

--- tests/integration/connect_test.go
+++ tests/integration/connect_test.go
@@ -1,1 +1,34 @@
 package pghelpers_test
+
+import (
+	_ "github.com/lib/pq" // Postgres
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	pgh "github.com/neighborly/go-pghelpers"
+)
+
+var _ = Describe("Connection Test", func() {
+
+	var (
+		testTracingConfig = pgh.PostgresTracingConfig{true, true,
+			true, true, true,
+			true, true}
+		//myTestString = "test"
+		testConfig = pgh.PostgresConfig{
+			Host:       "localhost",
+			Port:       5432,
+			Username:   "postgres",
+			Password:   "",
+			Database:   "postgres",
+			SSLEnabled: false,
+			Tracing:    testTracingConfig}
+	)
+
+	It("should connect to a database", func() {
+		db, err := pgh.ConnectPostgres(testConfig)
+		Expect(db).To(Not(BeNil()))
+		Expect(err).To(BeNil())
+		Expect(db.Ping()).To(Succeed())
+	})
+})
