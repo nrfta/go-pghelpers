170744ac749c13ac12f20d5c047b9791db8a626c
Krina <kparikh@neighborly.com>
2019-06-25 16:01:25 -0700

    Refactored go-pghelpers from go-errors

--- connect.go
+++ connect.go
@@ -6,11 +6,11 @@ import (
 	_ "github.com/lib/pq" // Postgres
 
 	"contrib.go.opencensus.io/integrations/ocsql"
-	"github.com/neighborly/gtoolbox/errors"
+	"github.com/neighborly/go-errors"
 )
 
 // ConnectPostgres connects to postgres
-func ConnectPostgres(c PostgresConfig) (*sql.DB, errors.Error) {
+func ConnectPostgres(c PostgresConfig) (*sql.DB, error) {
 	addr := c.GenerateAddress()
 
 	driver := "postgres"
@@ -30,17 +30,17 @@ func ConnectPostgres(c PostgresConfig) (*sql.DB, errors.Error) {
 
 		driver, err = ocsql.Register(driver, ocsql.WithOptions(tracingConfig))
 		if err != nil {
-			return nil, errors.New("Unable to enable tracing on postgres db").WithCause(err)
+			return nil, errors.Wrap(err, "Unable to enable tracing on postgres db")
 		}
 	}
 
 	db, err := sql.Open(driver, addr)
 	if err != nil {
-		return nil, errors.Newf("unable to open postgres db at %s:%d/%s", c.Host, c.Port, c.Database).WithCause(err)
+		return nil, errors.Wrapf(err, "unable to open postgres db at %s:%d/%s", c.Host, c.Port, c.Database)
 	}
 
 	if err := db.Ping(); err != nil {
-		return nil, errors.Newf("unable to ping postgres db at %s:%d/%s", c.Host, c.Port, c.Database).WithCause(err)
+		return nil, errors.Wrapf(err, "unable to ping postgres db at %s:%d/%s", c.Host, c.Port, c.Database)
 	}
 
 	return db, nil
--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require (
 	contrib.go.opencensus.io/integrations/ocsql v0.1.4
 	github.com/DATA-DOG/go-txdb v0.1.2
 	github.com/lib/pq v1.1.1
+	github.com/neighborly/go-errors v0.1.0
 	github.com/neighborly/gtoolbox v0.0.0-20190423222415-42505765c5c6
-	github.com/pkg/errors v0.8.1 // indirect
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.5.0
 	go.opencensus.io v0.22.0 // indirect
 )
